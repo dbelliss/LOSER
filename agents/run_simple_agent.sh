55d798d06d2f74d587840e65b9ce4ef717bfa4d8
Davey Jay Belliss <daveybelliss@gmail.com>
2018-05-10 11:06:46 -0700

    Features/basic agents (#3)
    * Adding simple_agent and smart_agent that plays as terran
    
    * Simple agent uses Zerg now

---
+++ agents/run_simple_agent.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python -m pysc2.bin.agent --map Simple64 --agent simple_agent.SimpleAgent --agent_race Z
---
+++ agents/run_smart_agent.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+## Run on map Simple64 using SmartAgent (with Q table) as Terran. Do not render graphics, and end the program after 5000 steps
+#python -m pysc2.bin.agent --map Simple64 --agent smart_agent.SmartAgent --agent_race T --norender --max_agent_steps 5000
+# Run on map Simple64 using SmartAgent (with Q table) as Terran, and end the program after 10000 steps
+python -m pysc2.bin.agent --map Simple64 --agent smart_agent.SmartAgent --agent_race T --max_agent_steps 0
